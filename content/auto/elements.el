fc18882b5927ab0b148c54631862a3558b1263e5
Jan Oliver Oelerich <janoliver@oelerich.org>
2012-05-07 20:13:06 +0200

    Changes

---
+++ content/auto/elements.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "elements"
+ (lambda ()
+    (LaTeX-add-labels
+     "cha:text-elements"
+     "sec:math"
+     "eq:eq1"
+     "sec:units"
+     "sec:figures"
+     "fig:bla"
+     "fig:bla2")))
+
---
+++ preamble/auto/docclass.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook "docclass"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "scrbook10"
+     "scrbook")))
+
